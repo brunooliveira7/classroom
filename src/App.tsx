8b8e6fd0c4897cdab9a70905187da94ceceba416
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-05-31 10:55:36 -0300

    feat: Add useMessage hook and integrate into App component
    This commit introduces a `useMessage` hook for displaying messages and integrates it into the App component.
    
    - Created `src/hooks/useMessage.ts` containing the `useMessage` hook, which takes a name and provides a `show` function to log messages with the given name.
    - Integrated the `useMessage` hook into `App.tsx`, instantiating it with the name "Bruno".
    - Added an `onClick` handler to the "Adicionar" button in `App.tsx` that calls the `show` function from the `useMessage` hook, displaying a message when the button is clicked.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./global.css";
 import { Button } from "./components/button";
 import styles from "./app.module.css";
+import { useMessage } from "./hooks/useMessage";
 
 export function App() {
+  const { show } = useMessage({name: "Bruno"});
+
   return (
     <div className={styles.container}>
-      <Button name="Adicionar" />
+      <Button name="Adicionar" onClick={() => show("Hook message")} />
       <span>0</span>
       <Button name="Excluir" />
     </div>
---
+++ src/hooks/useMessage.ts
@@ -0,0 +1,13 @@
+type Props = {
+  name: string;
+};
+
+export function useMessage({ name }: Props) {
+  function show(message: string) {
+    console.log(name, message);
+  }
+
+  return {
+    show,
+  };
+}
