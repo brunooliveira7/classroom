e12784a34aa7a9f6d739642feb5ca134aee94754
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-05-31 11:48:03 -0300

    feat: Implement counter functionality with React useState
    This commit introduces a counter feature to the App component using React's useState hook.
    
    - Imported the `useState` hook from React.
    - Initialized a `count` state variable with an initial value of 0 using `useState`.
    - Implemented `handleAdd` and `handleRemove` functions to increment and decrement the `count` state, respectively.
    - Added `onClick` handlers to the "Adicionar" and "Excluir" buttons to trigger the `handleAdd` and `handleRemove` functions.
    - Displayed the current `count` value within a `span` element.
    - Commented out the previous `useMessage` hook integration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import "./global.css";
+import { useState } from "react";
 import { Button } from "./components/button";
 import styles from "./app.module.css";
-import { useMessage } from "./hooks/useMessage";
+//import { useMessage } from "./hooks/useMessage";
 
 export function App() {
-  const { show } = useMessage({name: "Bruno"});
+  const [count, setCount] = useState(0);
 
+  //const { show } = useMessage({name: "Bruno"});
+
+  function handleAdd() {
+    setCount((prevState) => prevState + 1);
+  }
+
+  function handleRemove() {
+    setCount((prevState) => prevState - 1);
+  }
   return (
     <div className={styles.container}>
-      <Button name="Adicionar" onClick={() => show("Hook message")} />
-      <span>0</span>
-      <Button name="Excluir" />
+      <Button name="Adicionar" onClick={handleAdd} />
+      <span>{count}</span>
+      <Button name="Excluir" onClick={handleRemove} />
     </div>
   );
 }
